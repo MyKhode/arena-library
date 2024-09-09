3c2b79a57f759e011b4d191b16a32186ccc8eae4
Ikhode <132768132+MyKhode@users.noreply.github.com>
2024-09-09 15:47:01 +0700

    list user all

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,32 +2,32 @@ import { defineMiddleware } from "astro:middleware";
 import { supabase } from "../lib/supabase";
 import micromatch from "micromatch";
 
-const protectedRoutes = ["/", "/dashboard(|/)"];
+const protectedRoutes = ["/dashboard(|/)"];
 const redirectRoutes = ["/signin(|/)", "/register(|/)"];
-const protectedAPIRoutes = ["/api/guestbook(|/)"];
+const proptectedAPIRoutes = ["/api/guestbook(|/)"];
 
 export const onRequest = defineMiddleware(
   async ({ locals, url, cookies, redirect }, next) => {
-    // Check if the route is protected
     if (micromatch.isMatch(url.pathname, protectedRoutes)) {
       const accessToken = cookies.get("sb-access-token");
       const refreshToken = cookies.get("sb-refresh-token");
 
-      // If tokens are missing, redirect to sign-in
       if (!accessToken || !refreshToken) {
         return redirect("/signin");
       }
 
-      // Set session using tokens
       const { data, error } = await supabase.auth.setSession({
         refresh_token: refreshToken.value,
         access_token: accessToken.value,
       });
 
       if (error) {
-        // Handle token errors and redirect to sign-in
-        cookies.delete("sb-access-token", { path: "/" });
-        cookies.delete("sb-refresh-token", { path: "/" });
+        cookies.delete("sb-access-token", {
+          path: "/",
+        });
+        cookies.delete("sb-refresh-token", {
+          path: "/",
+        });
         return redirect("/signin");
       }
 
@@ -44,7 +44,6 @@ export const onRequest = defineMiddleware(
       });
     }
 
-    // Redirect to dashboard if already logged in
     if (micromatch.isMatch(url.pathname, redirectRoutes)) {
       const accessToken = cookies.get("sb-access-token");
       const refreshToken = cookies.get("sb-refresh-token");
@@ -54,18 +53,21 @@ export const onRequest = defineMiddleware(
       }
     }
 
-    // Protect specific API routes
-    if (micromatch.isMatch(url.pathname, protectedAPIRoutes)) {
+    if (micromatch.isMatch(url.pathname, proptectedAPIRoutes)) {
       const accessToken = cookies.get("sb-access-token");
       const refreshToken = cookies.get("sb-refresh-token");
 
+      // Check for tokens
       if (!accessToken || !refreshToken) {
         return new Response(
-          JSON.stringify({ error: "Unauthorized" }),
-          { status: 401 }
+          JSON.stringify({
+            error: "Unauthorized",
+          }),
+          { status: 401 },
         );
       }
 
+      // Verify the tokens
       const { error } = await supabase.auth.setSession({
         access_token: accessToken.value,
         refresh_token: refreshToken.value,
@@ -73,12 +75,14 @@ export const onRequest = defineMiddleware(
 
       if (error) {
         return new Response(
-          JSON.stringify({ error: "Unauthorized" }),
-          { status: 401 }
+          JSON.stringify({
+            error: "Unauthorized",
+          }),
+          { status: 401 },
         );
       }
     }
 
     return next();
-  }
+  },
 );
