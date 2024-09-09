4252f1098a0612e7e7b0ce291776512ca63d910a
Ikhode <132768132+MyKhode@users.noreply.github.com>
2024-09-09 11:29:11 +0700

    github action deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-supabase-auth.vercel.app",
+  site: "https://MyKhode.github.io",
   output: "server",
   adapter: vercel(),
   integrations: [tailwind(), solidJs()],
