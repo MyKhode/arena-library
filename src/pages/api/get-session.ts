8b24c816eb54a89cefccebbb1ff64f812cfc713e
Ikhode <132768132+MyKhode@users.noreply.github.com>
2024-09-13 15:53:28 +0700

    unity token with website

--- src/pages/api/get-session.ts
+++ src/pages/api/get-session.ts
@@ -4,18 +4,16 @@ export const GET: APIRoute = async ({ cookies }) => {
   const accessToken = cookies.get("sb-access-token");
   const refreshToken = cookies.get("sb-refresh-token");
 
-  console.log(`Access Token: ${accessToken}`);
-  console.log(`Refresh Token: ${refreshToken}`);
-
   if (!accessToken || !refreshToken) {
     return new Response("No session available", { status: 401 });
   }
 
-  return new Response(
-    JSON.stringify({
-      access_token: accessToken,
-      refresh_token: refreshToken,
-    }),
-    { status: 200 }
-  );
+  const deepLinkURL = `unitydl://login?access_token=${accessToken}&refresh_token=${refreshToken}`;
+
+  return new Response(JSON.stringify({ deepLinkURL }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 };
